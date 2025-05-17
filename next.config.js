a3082fb3c49d7b312ca8b4bb41fb6695693b8347
Eric Vaughan <openintro@gmail.com>
2025-05-16 22:57:36 -0500

    allow amazon s3 image urls

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['bell-registry-bucket.s3.us-east-2.amazonaws.com'],
     remotePatterns: [
       {
         protocol: 'https',
